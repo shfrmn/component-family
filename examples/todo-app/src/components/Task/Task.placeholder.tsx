0a81c225d9185d2287da186c2130b71b8ccb2697
Joseph Shaferman <joseph.shaferman@gmail.com>
2023-04-10 22:11:19 +0800

    Docs: Move placeholder styles to css

--- examples/todo-app/src/components/Task/Task.placeholder.tsx
+++ examples/todo-app/src/components/Task/Task.placeholder.tsx
@@ -1,15 +1,5 @@
+import style from "./Task.module.css"
+
 export function TaskPlaceholder(props: {}) {
-  return (
-    <div
-      style={{
-        marginBottom: "20px",
-        padding: "10px",
-        minWidth: "200px",
-        height: "20px",
-        // backgroundColor: "#eee",
-        border: "1px solid #ccc",
-        borderRadius: "0.5rem",
-      }}
-    />
-  )
+  return <div className={style.placeholder} />
 }
--- examples/todo-app/src/components/TaskList/TaskList.placeholder.tsx
+++ examples/todo-app/src/components/TaskList/TaskList.placeholder.tsx
@@ -1,11 +1,5 @@
-export function TaskListPlaceholder() {
-  return (
-    <div
-      style={{
-        minWidth: "240px",
-        height: "80vh",
-        backgroundColor: "#ccc",
-      }}
-    />
-  )
+import style from "./TaskList.module.css"
+
+export function TaskListPlaceholder(props: {}) {
+  return <div className={style.placeholder} />
 }
